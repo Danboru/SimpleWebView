cdc3043c1f6ae9743396e00e151028526c789ef2
Danboru <8danang8@gmail.com>
2017-06-02 21:47:34 +0700

    Stable Version

--- app/src/main/java/id/eightstudio/danboru/jogjaorganik/MainActivity.java
+++ app/src/main/java/id/eightstudio/danboru/jogjaorganik/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
@@ -14,8 +16,10 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import id.eightstudio.danboru.jogjaorganik.Problem.ConnectionProblem;
+import java.net.InetAddress;
 
 public class MainActivity extends Activity {
 
@@ -39,6 +43,13 @@ public class MainActivity extends Activity {
 
         view = (WebView) this.findViewById(R.id.webViewJogjaOrganic);
 
+        //Memeriksa ketersediaan internet
+        if (isInternetAvailable()){
+
+            Toast.makeText(this, "No Connection", Toast.LENGTH_SHORT).show();
+
+        } else {
+
             WebSettings settings = view.getSettings();
 
             //Set Local Storage enable
@@ -47,16 +58,15 @@ public class MainActivity extends Activity {
             //Can Running javascript
             view.getSettings().setJavaScriptEnabled(true);
             view.setWebViewClient(new MyBrowser());
-            view.loadUrl("http://jogjaorganik.com/livezilla/mobile/index.php"); //Site yang akan di tampilkan
+            view.loadUrl("http://google.com"); //Site yang akan di tampilkan
             view.setWebChromeClient(new WebChromeClient()); // adding js alert support
 
+        }
     }
 
     /***
-     *
      * Fungsi ini adalah fungsi untuk menangani saat link di clikc
      * idak membuka defaulth bwrowser
-     *
      * */
     private class MyBrowser extends WebViewClient {
         @Override
@@ -66,7 +76,21 @@ public class MainActivity extends Activity {
         }
     }
 
+    /***
+     * Fungsi ini akan mengembalikan true = ada koneksi, false = tidak ada koneksi
+     * */
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("http://google.com"); //You can replace it with your name
+            return !ipAddr.equals("");
 
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    //
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
         //Ketika disentuh tombol back
